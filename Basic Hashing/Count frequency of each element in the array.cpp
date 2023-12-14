5240034c490dd43f93821579ea862e37897a7db8
Siddharth Joisar <sidjoisar0807@gmail.com>
2023-12-14 22:25:21 +0530

    hashing

---
+++ Basic Hashing/Count frequency of each element in the array.cpp
@@ -0,0 +1,13 @@
+vector<int> countFrequency(int n, int x, vector<int> &nums)
+{
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] >= 1 && nums[i] <= n)
+        {
+            v[nums[i] - 1]++;
+        }
+    }
+    return v;
+}
