317678c6ac3e98547d0cac1f2bc56755691188ed
Siddharth Joisar <sidjoisar0807@gmail.com>
2024-04-08 23:55:41 +0530

    time needed to buy tickets

---
+++ 2] ARRAYS/Easy/Time Needed to Buy Tickets.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int timeRequiredToBuy(vector<int> &tickets, int k)
+    {
+        int ans = 0;              // Total time
+        int n = tickets.size();   // Size of ticket vector
+        int element = tickets[k]; // Price of ticket at index k
+
+        // Iterate through tickets
+        for (int i = 0; i < n; i++)
+        {
+            // If ticket index is <= k, add min of price of kth ticket and current ticket
+            if (i <= k)
+                ans += min(element, tickets[i]);
+            // If ticket index is > k, add min of (price of kth ticket - 1) and current ticket
+            else
+                ans += min(element - 1, tickets[i]);
+        }
+
+        return ans; // Return total time
+    }
+};
